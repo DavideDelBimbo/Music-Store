7f47f0f3b2183eae8418d2fbbe017397a05b9808
DavideDelBimbo <davide.delbimbo@edu.unifi.it>
2024-05-15 12:44:39 +0200

    Add picocli to handle arguments

--- io.github.davidedelbimbo.music-store/src/main/java/io/github/davidedelbimbo/music_store/app/swing/MusicStoreSwingApp.java
+++ io.github.davidedelbimbo.music-store/src/main/java/io/github/davidedelbimbo/music_store/app/swing/MusicStoreSwingApp.java
@@ -2,6 +2,12 @@ package io.github.davidedelbimbo.music_store.app.swing;
 
 import java.awt.EventQueue;
 
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
@@ -10,26 +16,43 @@ import io.github.davidedelbimbo.music_store.repository.mongo.MusicStoreMongoRepo
 import io.github.davidedelbimbo.music_store.view.swing.MusicStoreSwingView;
 import io.github.davidedelbimbo.music_store.view.swing.CreatePlaylistDialog;
 
-public class MusicStoreSwingApp {
+@Command(mixinStandardHelpOptions = true)
+public class MusicStoreSwingApp implements Callable<Void> {
+	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+
+	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
+	private Integer mongoPort = 27017;
+
+	@Option(names = { "--db-name" }, description = "Database name")
+	private String databaseName = "music_store";
+
+	@Option(names = { "--song-collection" }, description = "Song collection name")
+	private String songCollectionName = "songs";
+
+	@Option(names = { "--playlist-collection" }, description = "Playlist collection name")
+	private String playlistCollectionName = "playlists";
+
 	public static void main(String[] args) {
+		new CommandLine(new MusicStoreSwingApp()).execute(args);
+	}
+
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				String mongoHost = "localhost";
-				int mongoPort = 27017;
-				if (args.length > 0)
-					mongoHost = args[0];
-				if (args.length > 1)
-					mongoPort = Integer.parseInt(args[1]);
-				MusicStoreMongoRepository musicStoreRepository = new MusicStoreMongoRepository(new MongoClient(new ServerAddress(mongoHost, mongoPort)), "music_store", "songs", "playlists");
+				MongoClient client = new MongoClient(new ServerAddress(mongoHost, mongoPort));
+				MusicStoreMongoRepository musicStoreMongoRepository = new MusicStoreMongoRepository(client, databaseName, songCollectionName, playlistCollectionName);
 				MusicStoreSwingView musicStoreSwingView = new MusicStoreSwingView(new CreatePlaylistDialog());
-				MusicStoreController musicStoreController = new MusicStoreController(musicStoreSwingView, musicStoreRepository);
+				MusicStoreController musicStoreController = new MusicStoreController(musicStoreSwingView, musicStoreMongoRepository);
 				musicStoreSwingView.setMusicStoreController(musicStoreController);
 				musicStoreSwingView.setVisible(true);
 				musicStoreController.allSongs();
 				musicStoreController.allPlaylists();
-				} catch (Exception e) {
-					e.printStackTrace();
-					}
-			});
-		}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		return null;
+	}
 }
